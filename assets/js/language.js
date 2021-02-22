8fdcbe61a2ce41c023b50524866e42e9fe901ef2
HsiaoTienFan <hfan5@binghamton.edu>
2021-02-22 20:13:42 +1300

    add language toggle

---
+++ assets/js/language.js
@@ -0,0 +1,14 @@
+function toggleLanguage() {
+    document.getElementById("chinese").classList.toggle('on');
+    document.getElementById("english").classList.toggle('on');
+    document.getElementById("english-icon-index").classList.toggle('on');
+    document.getElementById("chinese-icon-index").classList.toggle('on');
+    document.getElementById("english-icon-about").classList.toggle('on');
+    document.getElementById("chinese-icon-about").classList.toggle('on');
+    document.getElementById("chinese").classList.toggle('off');
+    document.getElementById("english").classList.toggle('off');
+    document.getElementById("english-icon-index").classList.toggle('off');
+    document.getElementById("chinese-icon-index").classList.toggle('off');
+    document.getElementById("english-icon-about").classList.toggle('off');
+    document.getElementById("chinese-icon-about").classList.toggle('off');
+}
